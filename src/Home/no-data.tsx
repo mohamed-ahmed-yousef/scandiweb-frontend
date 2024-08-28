b2f06e140f4b9679c9bef1e0ffb89c671f57604d
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-08-29 01:10:09 +0300

    make error msg so clear & check responsiveness & add no data compnent

---
+++ src/Home/no-data.tsx
@@ -0,0 +1,15 @@
+import document from "./no-data.svg";
+import "./no-data.scss"; // Ensure your SCSS file is imported
+
+export function NoProductItem({
+	data,
+	isLoading,
+}: { data: string[]; isLoading: boolean }) {
+	if (data?.length || isLoading) return null;
+	return (
+		<div className="no-data">
+			<img alt={"No data"} src={document} className={"image"} />
+			<h1>No products. Add one to show.</h1>
+		</div>
+	);
+}
